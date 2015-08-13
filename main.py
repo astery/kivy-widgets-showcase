d38bf8e997badcb4a5f94a26475b815822efbe16
Andrey Akulov <andreyshark@gmail.com>
2015-08-13 14:50:29 +0300

    Extract filedialogs. Add save parameters to file button.

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.lang import Builder
+
+import os
 
 class ParamInput(BoxLayout):
     value = StringProperty('')
@@ -34,15 +38,57 @@ class ParamInputPopup(ParamInput):
 
 
 class ParamInputBrowseFile(ParamInput):
-    pass
+    file = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_choose(self):
+        content = ChooseDialog(choose=self.choose, cancel=self.dismiss_popup)
+        self._popup = Popup(title='Choose file', content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def choose(self, path, filename):
+        self.value = os.path.join(path, filename[0])
+        self.dismiss_popup()
+
+
+
+class ChooseDialog(FloatLayout):
+    choose = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class FormPanel(BoxLayout):
-    def send_params(self):
-        print('Params: ')
+    savefile = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title='Save file', content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.params_summary())
+
+        self.dismiss_popup()
+
+    def params_summary(self):
+        output = []
+        output.append('Params: ')
         for key, param in self.ids.items():
             if isinstance(param, ParamInput):
-                print("{0}: {1}".format(key, param.get_value()))
+                output.append("{0}: {1}".format(key, param.get_value()))
+        return os.linesep.join(output)
 
 
 class ShowcaseApp(App):
